85fd60bfe4eebe61ff25d7ea6b23f7da1b30d904
Matthew Landauer <mlandauer@gmail.com>
2019-05-17 04:26:50 +1000

    Extract method

--- spec/epathway_scraper_spec.rb
+++ spec/epathway_scraper_spec.rb
@@ -5,13 +5,13 @@ RSpec.describe EpathwayScraper do
     expect(EpathwayScraper::VERSION).not_to be nil
   end
 
-  it "South_Gippsland_Shire_DAs" do
+  def test_scraper(scraper_name:, base_url:, index:)
     scraper = EpathwayScraper::Scraper.new(
-      base_url: 'https://eservices.southgippsland.vic.gov.au/ePathway/ePathProd/Web/GeneralEnquiry/EnquiryLists.aspx?ModuleCode=LAP',
-      index: 0
+      base_url: base_url,
+      index: index
     )
 
-    results = VCR.use_cassette("South_Gippsland_Shire_DAs") do
+    results = VCR.use_cassette(scraper_name) do
       Timecop.freeze(Date.new(2019,5,14)) do
         results = []
         scraper.scrape do |record|
@@ -21,29 +21,24 @@ RSpec.describe EpathwayScraper do
       end
     end
 
-    expected = YAML.load(File.read("fixtures/expected/South_Gippsland_Shire_DAs.yml"))
+    expected = YAML.load(File.read("fixtures/expected/#{scraper_name}.yml"))
 
     expect(results).to eq expected
   end
 
+  it "South_Gippsland_Shire_DAs" do
+    test_scraper(
+      scraper_name: "South_Gippsland_Shire_DAs",
+      base_url: 'https://eservices.southgippsland.vic.gov.au/ePathway/ePathProd/Web/GeneralEnquiry/EnquiryLists.aspx?ModuleCode=LAP',
+      index: 0
+    )
+  end
+
   it "ballarat" do
-    scraper = EpathwayScraper::Scraper.new(
+    test_scraper(
+      scraper_name: "ballarat",
       base_url: "https://eservices.ballarat.vic.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquiryLists.aspx?ModuleCode=LAP",
       index: 0
     )
-
-    results = VCR.use_cassette("ballarat") do
-      Timecop.freeze(Date.new(2019,5,14)) do
-        results = []
-        scraper.scrape do |record|
-          results << record
-        end
-        results.sort_by{|r| r["council_reference"]}
-      end
-    end
-
-    expected = YAML.load(File.read("fixtures/expected/ballarat.yml"))
-
-    expect(results).to eq expected
   end
 end
