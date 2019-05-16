7798a978646d0af7e11a5ef4adab8aa305b02366
Matthew Landauer <mlandauer@gmail.com>
2019-05-17 04:22:57 +1000

    Test doesn't write to sqlite and add ballarat regression test

--- spec/epathway_scraper_spec.rb
+++ spec/epathway_scraper_spec.rb
@@ -1,10 +1,6 @@
 require "timecop"
 
 RSpec.describe EpathwayScraper do
-  before(:each) do
-    File.delete("./data.sqlite") if File.exist?("./data.sqlite")
-  end
-
   it "has a version number" do
     expect(EpathwayScraper::VERSION).not_to be nil
   end
@@ -15,14 +11,38 @@ RSpec.describe EpathwayScraper do
       index: 0
     )
 
-    VCR.use_cassette("South_Gippsland_Shire_DAs") do
+    results = VCR.use_cassette("South_Gippsland_Shire_DAs") do
       Timecop.freeze(Date.new(2019,5,14)) do
-        scraper.scrape_and_save
+        results = []
+        scraper.scrape do |record|
+          results << record
+        end
+        results.sort_by{|r| r["council_reference"]}
       end
     end
 
     expected = YAML.load(File.read("fixtures/expected/South_Gippsland_Shire_DAs.yml"))
-    results = ScraperWiki.select("* from data order by council_reference")
+
+    expect(results).to eq expected
+  end
+
+  it "ballarat" do
+    scraper = EpathwayScraper::Scraper.new(
+      base_url: "https://eservices.ballarat.vic.gov.au/ePathway/Production/Web/GeneralEnquiry/EnquiryLists.aspx?ModuleCode=LAP",
+      index: 0
+    )
+
+    results = VCR.use_cassette("ballarat") do
+      Timecop.freeze(Date.new(2019,5,14)) do
+        results = []
+        scraper.scrape do |record|
+          results << record
+        end
+        results.sort_by{|r| r["council_reference"]}
+      end
+    end
+
+    expected = YAML.load(File.read("fixtures/expected/ballarat.yml"))
 
     expect(results).to eq expected
   end
