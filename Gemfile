cff33d5bdea26c2acac8b908d83f3d098e6546b1
Matthew Landauer <mlandauer@gmail.com>
2019-05-17 04:03:55 +1000

    Copy across one of the regression tests

--- Gemfile
+++ Gemfile
@@ -4,3 +4,6 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
 # Specify your gem's dependencies in epathway_scraper.gemspec
 gemspec
+
+# Make sure that we use our version of the scraperwiki gem here
+gem 'scraperwiki', git: 'https://github.com/openaustralia/scraperwiki-ruby.git', branch: 'morph_defaults'
--- spec/epathway_scraper_spec.rb
+++ spec/epathway_scraper_spec.rb
@@ -1,9 +1,29 @@
+require "timecop"
+
 RSpec.describe EpathwayScraper do
+  before(:each) do
+    File.delete("./data.sqlite") if File.exist?("./data.sqlite")
+  end
+
   it "has a version number" do
     expect(EpathwayScraper::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "South_Gippsland_Shire_DAs" do
+    scraper = EpathwayScraper::Scraper.new(
+      base_url: 'https://eservices.southgippsland.vic.gov.au/ePathway/ePathProd/Web/GeneralEnquiry/EnquiryLists.aspx?ModuleCode=LAP',
+      index: 0
+    )
+
+    VCR.use_cassette("South_Gippsland_Shire_DAs") do
+      Timecop.freeze(Date.new(2019,5,14)) do
+        scraper.scrape_and_save
+      end
+    end
+
+    expected = YAML.load(File.read("fixtures/expected/South_Gippsland_Shire_DAs.yml"))
+    results = ScraperWiki.select("* from data order by council_reference")
+
+    expect(results).to eq expected
   end
 end
