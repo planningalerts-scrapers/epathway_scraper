da54eb6a5ace6dc8d7a91351e4196eacdb508392
Matthew Landauer <mlandauer@gmail.com>
2019-05-29 11:26:36 +1000

    Move method

--- lib/epathway_scraper.rb
+++ lib/epathway_scraper.rb
@@ -67,39 +67,7 @@ module EpathwayScraper
     end
 
     def extract_index_data(row)
-      result = {
-        council_reference: row[:content]["App No."] ||
-                           row[:content]["Application Number"] ||
-                           row[:content]["Application number"],
-        address: row[:content]["Location Address"] ||
-                 row[:content]["Property Address"] ||
-                 row[:content]["Site Location"] ||
-                 row[:content]["Application location"] ||
-                 row[:content]["Application Location"] ||
-                 row[:content]["Location"] ||
-                 row[:content]["Primary Property Address"] ||
-                 row[:content]["Site Address"] ||
-                 (if row[:content]["Address"] && row[:content]["Suburb"]
-                    (row[:content]["Address"] + ", " + row[:content]["Suburb"] + ", VIC")
-                  end) ||
-                 row[:content]["Address"],
-        description: row[:content]["Proposed Use or Development"] ||
-                     row[:content]["Description"] ||
-                     row[:content]["Application Proposal"] ||
-                     row[:content]["Proposal"] ||
-                     row[:content]["Application Description"] ||
-                     row[:content]["Application proposal"],
-        # This URL will only work in a session. Thanks for that!
-        detail_url: row[:url]
-      }
-      date_received = row[:content]["Date Lodged"] ||
-                      row[:content]["Date lodged"] ||
-                      row[:content]["Application Date"] ||
-                      row[:content]["Lodgement Date"] ||
-                      row[:content]["Date received"] ||
-                      row[:content]["Date"]
-      result[:date_received] = Date.strptime(date_received, "%d/%m/%Y").to_s if date_received
-      result
+      Page::Index.extract_index_data(row)
     end
 
     # list_type one of :advertising, :all, :last_30_days
