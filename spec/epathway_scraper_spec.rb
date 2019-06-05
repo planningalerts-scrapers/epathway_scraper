3fc63209836ab2e720c253752ab6690f21f0d963
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 13:31:11 +1000

    Extract method

--- spec/epathway_scraper_spec.rb
+++ spec/epathway_scraper_spec.rb
@@ -17,8 +17,12 @@ RSpec.describe EpathwayScraper do
   end
 
   describe "Scraper" do
-    def test_scraper(scraper_name, params)
-      results = VCR.use_cassette(scraper_name) do
+    def test_scraper2(authority)
+      test_scraper(authority, EpathwayScraper::AUTHORITIES[authority])
+    end
+
+    def test_scraper(authority, params)
+      results = VCR.use_cassette(authority) do
         Timecop.freeze(Date.new(2019, 5, 15)) do
           results = []
 
@@ -30,8 +34,8 @@ RSpec.describe EpathwayScraper do
         end
       end
 
-      expected = if File.exist?("fixtures/expected/#{scraper_name}.yml")
-                   YAML.safe_load(File.read("fixtures/expected/#{scraper_name}.yml"))
+      expected = if File.exist?("fixtures/expected/#{authority}.yml")
+                   YAML.safe_load(File.read("fixtures/expected/#{authority}.yml"))
                  else
                    []
                  end
@@ -39,7 +43,7 @@ RSpec.describe EpathwayScraper do
       if results != expected
         # Overwrite expected so that we can compare with version control
         # (and maybe commit if it is correct)
-        File.open("fixtures/expected/#{scraper_name}.yml", "w") do |f|
+        File.open("fixtures/expected/#{authority}.yml", "w") do |f|
           f.write(results.to_yaml)
         end
       end
@@ -47,9 +51,9 @@ RSpec.describe EpathwayScraper do
       expect(results).to eq expected
     end
 
-    EpathwayScraper::AUTHORITIES.each do |scraper_name, params|
-      it scraper_name do
-        test_scraper(scraper_name, params)
+    EpathwayScraper::AUTHORITIES.keys.each do |authority|
+      it authority do
+        test_scraper2(authority)
       end
     end
   end
